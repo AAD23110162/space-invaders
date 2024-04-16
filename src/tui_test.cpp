2deef5f98db2f4f6423826339ea6a230dae0fb41
AAD23110162 <159966137+AAD23110162@users.noreply.github.com>
2024-04-16 00:20:33 +0000

    Version 1

---
+++ src/tui_test.cpp
@@ -0,0 +1,25 @@
+
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/screen/string.hpp>
+
+using namespace ftxui;
+
+int main(int argc, char const *argv[])
+{
+    //Crea los elementos de la interfaz de usuario
+    Element label=text("Hola Mundo");
+    Element container=hbox({label});
+
+    //Crea la pantalla
+    auto screen=Screen::Create
+    {
+        Dimension::Full(),
+        Dimension::Fit(container)
+
+    };
+    Render (screen,container);
+    screen.Print();
+    /* code */
+    return 0;
+}
